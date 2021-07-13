4ea0889862cffa4d4ca6f284b8145067f997b191
kilchenmann <kilchenmann@users.noreply.github.com>
2021-07-13 19:36:27 +0000

    Deployed 1637bd9 with MkDocs version: 1.1.2

---
+++ DSP-API/src/api-v1/sampleRequests/sampleGraphDataResponse.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2015-2021 the contributors (see Contributors.md).
+ *
+ * This file is part of DSP — DaSCH Service Platform.
+ *
+ * DSP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DSP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with DSP. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { graphDataResponseFormats } from "../graphDataResponseFormats"
+
+let graphDataResponse: graphDataResponseFormats.graphDataResponse = {"nodes":[{"resourceIri":"http://rdfh.ch/sHCLAGg-R5qJ6oPZPV-zOQ","resourceLabel":"Golf","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"},{"resourceIri":"http://rdfh.ch/iqW_PBiHRdyTFzik8tuSog","resourceLabel":"Quebec","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"},{"resourceIri":"http://rdfh.ch/L5xU7Qe5QUu6Wz3cDaCxbA","resourceLabel":"Papa","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"},{"resourceIri":"http://rdfh.ch/A67ka6UQRHWf313tbhQBjw","resourceLabel":"Victor","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"},{"resourceIri":"http://rdfh.ch/tPfZeNMvRVujCQqbIbvO0A","resourceLabel":"Echo","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"},{"resourceIri":"http://rdfh.ch/Lz7WEqJETJqqsUZQYexBQg","resourceLabel":"Foxtrot","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"},{"resourceIri":"http://rdfh.ch/anything/start","resourceLabel":"Romeo","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"},{"resourceIri":"http://rdfh.ch/0C-0L1kORryKzJAJxxRyRQ","resourceLabel":"Sierra","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"},{"resourceIri":"http://rdfh.ch/WLSHxQUgTOmG1T0lBU2r5w","resourceLabel":"Tango","resourceClassIri":"http://www.knora.org/ontology/0001/anything#Thing","resourceClassLabel":"Thing"}],"edges":[{"source":"http://rdfh.ch/anything/start","target":"http://rdfh.ch/0C-0L1kORryKzJAJxxRyRQ","propertyIri":"http://www.knora.org/ontology/0001/anything#hasOtherThing","propertyLabel":"Another thing"},{"source":"http://rdfh.ch/tPfZeNMvRVujCQqbIbvO0A","target":"http://rdfh.ch/Lz7WEqJETJqqsUZQYexBQg","propertyIri":"http://www.knora.org/ontology/0001/anything#hasOtherThing","propertyLabel":"Another thing"},{"source":"http://rdfh.ch/anything/start","target":"http://rdfh.ch/WLSHxQUgTOmG1T0lBU2r5w","propertyIri":"http://www.knora.org/ontology/0001/anything#hasOtherThing","propertyLabel":"Another thing"},{"source":"http://rdfh.ch/Lz7WEqJETJqqsUZQYexBQg","target":"http://rdfh.ch/sHCLAGg-R5qJ6oPZPV-zOQ","propertyIri":"http://www.knora.org/ontology/0001/anything#hasOtherThing","propertyLabel":"Another thing"},{"source":"http://rdfh.ch/anything/start","target":"http://rdfh.ch/tPfZeNMvRVujCQqbIbvO0A","propertyIri":"http://www.knora.org/ontology/0001/anything#hasOtherThing","propertyLabel":"Another thing"},{"source":"http://rdfh.ch/L5xU7Qe5QUu6Wz3cDaCxbA","target":"http://rdfh.ch/iqW_PBiHRdyTFzik8tuSog","propertyIri":"http://www.knora.org/ontology/0001/anything#hasOtherThing","propertyLabel":"Another thing"},{"source":"http://rdfh.ch/WLSHxQUgTOmG1T0lBU2r5w","target":"http://rdfh.ch/A67ka6UQRHWf313tbhQBjw","propertyIri":"http://www.knora.org/ontology/0001/anything#hasOtherThing","propertyLabel":"Another thing"},{"source":"http://rdfh.ch/iqW_PBiHRdyTFzik8tuSog","target":"http://rdfh.ch/anything/start","propertyIri":"http://www.knora.org/ontology/0001/anything#hasOtherThing","propertyLabel":"Another thing"}],"status":0}
