abaa264f55fac08390dabc1d8e37f0aa5fe420d9
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2023-10-20 14:11:19 +0200

    bump to 2023.10.02

--- release.mk
+++ release.mk
@@ -1,6 +1,6 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2023.10.01"
-API := "v30.3.0"
-APP := "v10.23.7"
-TOOLS := "v5.0.2"
+DSP := "2023.10.02"
+API := "v30.4.0"
+APP := "v10.24.2"
+TOOLS := "v5.0.3"
