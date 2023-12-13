aa3828c900a088db294d23a6d1df0586f1acc6f4
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2023-12-13 09:38:23 +0100

    bump to 2023.12.01

--- release.mk
+++ release.mk
@@ -1,6 +1,6 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2023.11.01"
-API := "v30.5.1"
-APP := "v11.1.1"
-TOOLS := "v5.2.0"
+DSP := "2023.12.01"
+API := "v30.6.0"
+APP := "v11.1.7"
+TOOLS := "v5.5.0"
