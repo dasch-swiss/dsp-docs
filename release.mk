d6b98274b87f840bf78b65cf6a10f28c7df197c5
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2024-02-14 14:45:52 +0100

    Update docs 2024.02.01 (#101)

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2024.01.01"
-API := "v30.7.0"
-APP := "v11.2.0"
-TOOLS := "v5.7.0"
-INGEST := "v0.6.0"
+DSP := "2024.02.01"
+API := "v30.8.1"
+APP := "v11.6.2"
+TOOLS := "v5.8.1"
+INGEST := "v0.6.1"
