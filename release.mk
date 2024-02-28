b4ce6b01dd8194dfc3ada4c598664a3d85f4eb97
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-02-28 09:56:05 +0100

    edit

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2024.02.01"
-API := "v30.8.1"
-APP := "v11.6.2"
-TOOLS := "v5.8.1"
-INGEST := "v0.6.1"
+DSP := "2024.02.02"
+API := "v30.8.2"
+APP := "v11.6.5"
+TOOLS := "v6.0.0"
+INGEST := "v0.6.2"
