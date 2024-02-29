ce84a131c0c0b571c8ee5df2bfb9ec1febf5d468
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2024-02-29 16:33:28 +0100

    fix markdown and dead links in DSP-APP and DSP-API (#104)

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
 DSP := "2024.02.02"
-API := "0370e13d777e0f2b69c7560b6efbd7d26057537d"
-APP := "v11.6.5"
+API := "068ec841633ba0df1c32be3a4fbdea3f4700980a"
+APP := "4558e343aa5b3f693e925bc295c8e9e8e59c5f8e"
 TOOLS := "v6.0.0"
 INGEST := "v0.6.2"
