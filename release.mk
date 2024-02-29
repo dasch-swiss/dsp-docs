d5da80a55b1013fbb39ecebcd9d20eb129571200
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2024-02-29 14:02:27 +0100

    fix

--- release.mk
+++ release.mk
@@ -2,6 +2,6 @@
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
 DSP := "2024.02.02"
 API := "0370e13d777e0f2b69c7560b6efbd7d26057537d"
-APP := "v11.6.5"
+APP := "4e993900defe0ae322185e77a08f8e3d97866dfe"
 TOOLS := "v6.0.0"
 INGEST := "v0.6.2"
