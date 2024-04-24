beb6670404e3271c461a694ecd9a0a140647b649
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2024-04-24 10:37:16 +0200

    bump to 2024.04.02 (#111)

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2024.03.02"
-API := "v30.10.1"
-APP := "v11.7.5"
-TOOLS := "v7.1.1"
-INGEST := "v0.8.1"
+DSP := "2024.04.02"
+API := "v30.12.0"
+APP := "v11.9.0"
+TOOLS := "v7.1.3"
+INGEST := "v0.8.3"
