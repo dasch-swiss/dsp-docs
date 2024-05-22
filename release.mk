b18fb756c8b2d33e2d1e3f241bb6849b91f30d52
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2024-05-22 09:28:15 +0200

    chore: bump to 2024.05.02 (#113)

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2024.05.01"
-API := "v30.13.0"
+DSP := "2024.05.02"
+API := "v30.14.0"
 APP := "v11.9.1"
-TOOLS := "v7.2.0"
-INGEST := "v0.9.0"
+TOOLS := "v8.0.0"
+INGEST := "v0.9.1"
