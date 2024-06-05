4c3759ae2b1de287329281392708cfee1be88e9b
gNahcab <gregor.bachmann@dasch.swiss>
2024-06-05 14:39:00 +0200

    udpate docs

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2024.05.02"
-API := "v30.14.0"
-APP := "v11.9.1"
-TOOLS := "v8.0.0"
-INGEST := "v0.9.1"
+DSP := "2024.05.03"
+API := "v30.15.0"
+APP := "v11.10.0"
+TOOLS := "v8.1.0"
+INGEST := "v0.10.0"
