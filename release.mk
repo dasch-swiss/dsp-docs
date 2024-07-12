17d4d47e0e1b0994cff1ed72f5bc856953786bbb
Gregor Bachmann <91829513+gNahcab@users.noreply.github.com>
2024-07-12 10:33:58 +0200

    chore: bump to 2024.07.01 (#119)

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
-# for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/roles/dsp-deploy/files/RELEASE.json
+# for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2024.06.01"
-API := "v30.16.0"
-APP := "v11.12.0"
-TOOLS := "v8.2.0"
-INGEST := "v0.10.1"
+DSP := "2024.07.01"
+API := "v30.17.1"
+APP := "v11.14.0"
+TOOLS := "v8.3.0"
+INGEST := "v0.11.0"
