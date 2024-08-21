789f0802b4523333a5f259a76e026edfb4d9e1dd
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2024-08-21 12:17:47 +0300

    Deploy 2024.08.01 (#131)

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2024.07.02"
-API := "v30.18.2"
-APP := "v11.16.3"
-TOOLS := "v8.4.0"
-INGEST := "v0.12.0"
+DSP := "2024.08.01"
+API := "v30.18.3"
+APP := "v11.17.1"
+TOOLS := "v8.5.0"
+INGEST := "v0.12.2"
