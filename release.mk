24d7c3f775b63966eb7766d434a3bf3025f25dd0
Irmantas Kaukas <irmastnt@gmail.com>
2024-09-03 12:45:51 +0200

    chore: DSP 2024.08.02

--- release.mk
+++ release.mk
@@ -1,7 +1,7 @@
 # for DSP, API, APP, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-DSP := "2024.08.01"
-API := "v30.18.3"
-APP := "v11.17.1"
-TOOLS := "ff0b1de7eb9e146c659774454125b20f4cb85cf9"
-INGEST := "v0.12.2"
+DSP := "2024.08.02"
+API := "v30.18.4"
+APP := "v11.18.2"
+TOOLS := "268055603642ada6212c0bfae3ef94a3efea6573"
+INGEST := "v0.12.3"
