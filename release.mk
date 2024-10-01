3239cc524fa70d3a6a5da2c8af4e37762a3f9d6d
Julien Schneider <julien.schneider.1991@gmail.com>
2024-10-01 15:12:30 +0200

    chore: release 2024.09.01

--- release.mk
+++ release.mk
@@ -1,10 +1,10 @@
 # for DSP, API, APP, Ingest, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
 # for META, see https://github.com/dasch-swiss/dsp-meta/releases (NB: tags start with 'dsp-meta-')
-DSP := "2024.08.02"
-API := "v30.18.4"
-APP := "v11.18.2"
-TOOLS := "v9.0.2"
-INGEST := "v0.12.3"
+DSP := "2024.09.01"
+API := "v30.19.0"
+APP := "v11.20.4"
+TOOLS := "v9.1.0"
+INGEST := "v0.13.0"
 META := "dsp-meta-v2.3.0"
 
