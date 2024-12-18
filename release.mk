92a1fd6b399cf137ac998adae07496a14f4f92f5
Marcin Procyk <marcin.procyk@dasch.swiss>
2024-12-18 16:07:06 +0100

    bump docs to 2024.12.01

--- release.mk
+++ release.mk
@@ -1,9 +1,9 @@
 # for DSP, API, APP, Ingest, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
 # for META, see https://github.com/dasch-swiss/dsp-meta/tags (NB: tags start with 'dsp-meta-')
-DSP := "2024.11.01"
-API := "v30.21.0"
-APP := "v11.22.1"
-TOOLS := "v9.2.0"
-INGEST := "v0.14.1"
-META := "dsp-meta-v2.3.2"
+DSP := "2024.12.01"
+API := "v31.1.0"
+APP := "v11.22.4"
+TOOLS := "v10.0.0"
+INGEST := "v0.16.0"
+META := "dsp-meta-v2.3.3"
