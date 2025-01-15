220e429a85f4507cc2fd193a03d3877a3efb49ea
Marcin Procyk <marcin.procyk@dasch.swiss>
2025-01-15 10:20:09 +0100

    deploy: bump docs to 2025.01.01

--- release.mk
+++ release.mk
@@ -1,9 +1,9 @@
 # for DSP, API, APP, Ingest, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
 # for META, see https://github.com/dasch-swiss/dsp-meta/tags (NB: tags start with 'dsp-meta-')
-DSP := "2024.12.01"
-API := "v31.1.0"
-APP := "v11.22.4"
-TOOLS := "v10.0.0"
-INGEST := "v0.16.0"
+DSP := "2025.01.01"
+API := "v31.2.0"
+APP := "v11.22.5"
+TOOLS := "v10.1.0"
+INGEST := "v0.17.0"
 META := "dsp-meta-v2.3.3"
