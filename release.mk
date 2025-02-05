8afb8c329a610072ac5692c65194a7145b2d6b9e
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2025-02-05 09:15:48 +0100

    Update outdated link

--- release.mk
+++ release.mk
@@ -1,6 +1,6 @@
 # for DSP, API, APP, Ingest, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
-# for META, see https://github.com/dasch-swiss/dsp-meta/releases (NB: tags start with 'dsp-meta-')
+# for META, see https://github.com/dasch-swiss/dsp-meta/tags (NB: tags start with 'dsp-meta-')
 DSP := "2025.02.01"
 API := "v31.4.0"
 APP := "v11.25.0"
