c51fa0ec046b1b790cc231fc651ee5d50ebe34f5
Julien Schneider <julien.schneider.1991@gmail.com>
2025-04-02 10:58:28 +0200

    feat: adjust versions to 2025.04.01

--- release.mk
+++ release.mk
@@ -1,10 +1,10 @@
 # for DSP, API, APP, Ingest, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
 # for META, see https://github.com/dasch-swiss/dsp-meta/releases (NB: tags start with 'dsp-meta-')
-DSP := "2024.09.01"
-API := "v30.19.0"
-APP := "v11.20.4"
-TOOLS := "v9.1.0"
-INGEST := "v0.13.0"
-META := "dsp-meta-v2.3.0"
+DSP := "2025.04.01"
+API := "v31.10.0"
+APP := "v11.27.0"
+TOOLS := "v12.1.0"
+INGEST := "v0.19.0"
+META := "dsp-meta-v2.3.7"
 
