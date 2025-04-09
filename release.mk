4c3fe2bb20e222bc7e44842219bf9a9147712812
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2025-04-09 12:01:43 +0200

    deploy: bump docs to 2025.04.02 (#157)

--- release.mk
+++ release.mk
@@ -2,9 +2,9 @@
 # for DSP, API, APP, Ingest, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
 # for META, see https://github.com/dasch-swiss/dsp-meta/tags (NB: tags start with 'dsp-meta-')
-DSP := "2025.04.01"
+DSP := "2025.04.02"
 API := "v31.10.0"
-APP := "v11.27.0"
-TOOLS := "v12.1.0"
+APP := "v11.29.0"
+TOOLS := "v13.0.0"
 INGEST := "v0.19.0"
-META := "dsp-meta-v2.3.7"
+META := "dsp-meta-v2.3.8"
