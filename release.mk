de9925792a45f41360d558a0d719ad9c1be03e8f
Nora-Olivia-Ammann <103038637+Nora-Olivia-Ammann@users.noreply.github.com>
2025-07-09 10:48:11 +0200

    deploy: bump docs to 2025.07.02 (#166)

--- release.mk
+++ release.mk
@@ -2,9 +2,9 @@
 # for DSP, API, APP, Ingest, see https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
 # for TOOLS, see https://github.com/dasch-swiss/dsp-tools/releases
 # for META, see https://github.com/dasch-swiss/dsp-meta/tags (NB: tags start with 'dsp-meta-')
-DSP := "2025.07.01"
-API := "v31.19.0"
-APP := "v11.35.0"
-TOOLS := "v16.2.0"
-INGEST := "v0.20.0"
+DSP := "2025.07.02"
+API := "v31.19.1"
+APP := "v11.35.1"
+TOOLS := "v16.3.0"
+INGEST := "v0.20.1"
 META := "dsp-meta-v2.3.10"
