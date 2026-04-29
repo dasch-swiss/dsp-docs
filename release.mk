90fc9272c7fb74d85be74011a3af08caab812ac4
Julien Schneider <julien.schneider.1991@gmail.com>
2026-04-29 16:47:37 +0200

    deploy: bump docs to 2026.04.03 (#205)

--- release.mk
+++ release.mk
@@ -1,5 +1,5 @@
-DSP := "2026.04.02"
-API := "v35.6.0"  # copy-paste from https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
-APP := "v13.1.1"  # copy-paste from https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
-TOOLS := "v18.12.0"  # copy-paste from https://github.com/dasch-swiss/dsp-tools/releases
-META := "dsp-meta-v2.4.15"  # copy-paste from https://github.com/dasch-swiss/dsp-meta/tags (NB: tags start with 'dsp-meta-')
+DSP := "2026.04.03"
+API := "v35.8.0"  # copy-paste from https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
+APP := "v13.2.0"  # copy-paste from https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
+TOOLS := "v18.13.0"  # copy-paste from https://github.com/dasch-swiss/dsp-tools/releases
+META := "dsp-meta-v2.4.16"  # copy-paste from https://github.com/dasch-swiss/dsp-meta/tags (NB: tags start with 'dsp-meta-')
