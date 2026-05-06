98b81a403432843f81dd050d02874a878b4ed959
Marcin Procyk <marcin.procyk@dasch.swiss>
2026-05-06 11:10:00 +0200

    deploy: bump docs to DSP 2026.05.01 (#206)

--- release.mk
+++ release.mk
@@ -1,5 +1,5 @@
-DSP := "2026.04.03"
-API := "v35.8.0"  # copy-paste from https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
-APP := "v13.2.0"  # copy-paste from https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
-TOOLS := "v18.13.0"  # copy-paste from https://github.com/dasch-swiss/dsp-tools/releases
+DSP := "2026.05.01"
+API := "v35.8.1"  # copy-paste from https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
+APP := "v13.3.0"  # copy-paste from https://github.com/dasch-swiss/ops-deploy/blob/main/versions/RELEASE.json
+TOOLS := "v18.14.0 "  # copy-paste from https://github.com/dasch-swiss/dsp-tools/releases
 META := "dsp-meta-v2.4.16"  # copy-paste from https://github.com/dasch-swiss/dsp-meta/tags (NB: tags start with 'dsp-meta-')
